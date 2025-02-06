de29086d03687a8927f3df4ed85327dcf8608203
Ken Tabo <ken.tabo@student.ateneo.edu>
2025-02-06 15:47:36 +0800

    - added home app - made first view

---
+++ home/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Sticking out your gyatt for the rizzler\nUr so skibidi\n Ur so fanum tax\nI just wanna be ur sigma\nFricking come here\nGimme your Ohio")    
